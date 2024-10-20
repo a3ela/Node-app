45fca3791aa25cb99fc53cd18f9cd249e7841d40
a3ela <abelsintayehu626@gmail.com>
2024-10-20 19:52:51 +0300

    database of mongodb added

--- server/index.js
+++ server/index.js
@@ -1,70 +1,61 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const Note = require("./models/note");
 const app = express();
-
-// Load environment variables from .env file
-dotenv.config();
 
 // mideleware
 app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"));
 
+// the following code is for the frontend tester
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
-/// getting all notes
+// getting all notes
 app.get("/api/notes", (req, res) => {
   Note.find({}).then((notes) => {
     res.json(notes);
   });
 });
 
-/// getting a single note
+// getting a single note
 app.get("/api/notes/:id", (req, res) => {
   const id = req.params.id;
-  const note = notes.find((note) => note.id === id);
-  if (note) {
+  Note.findById(id).then((note) => {
     res.json(note);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
-/// delete a single note
+// delete a single note
 app.delete("/api/notes/:id", (req, res) => {
   const id = req.params.id;
   notes = notes.filter((note) => note.id !== id);
   res.status(204).end();
 });
 
-/// create a new note
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
+// create a new note
 app.post("/api/notes", (req, res) => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  const note = req.body;
+  const body = req.body;
 
   if (!body.content) {
-    return express.response.status(400).json({
+    return response.status(400).json({
       error: "content missing",
     });
   }
 
-  note = {
-    id: generateId(),
-    content: note.content,
-    important: note.important || false,
-  };
-  notes = notes.concat(note);
-  res.json(note);
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    createAt: new Date(),
+  });
+
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
