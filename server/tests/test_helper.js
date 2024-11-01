ffb9e71dd471a64fc750f381008a4c8db4eaf06a
abel <abelsintayehu626@gmail.com>
2024-11-01 18:25:11 +0300

    test file for api

---
+++ server/tests/test_helper.js
@@ -0,0 +1,31 @@
+const Note = require("../models/note");
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    important: false,
+  },
+  {
+    content: "Browser can excute only js",
+    important: true,
+  },
+];
+
+const nonExistingId = async () => {
+  const note = new Note({ content: "willremovethislater" });
+  await note.save();
+  await note.deleteOne();
+
+  return note._id.toString();
+};
+
+const notesInDb = async () => {
+  const notes = await Note.find({});
+  return notes.map((n) => n.toJSON());
+};
+
+module.exports = {
+  initialNotes,
+  nonExistingId,
+  notesInDb,
+};
